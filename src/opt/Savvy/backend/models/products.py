0efba27ca902eaa95572ce6fe8774ef013a5285b
Colin McIntosh <colin@colinmcintosh.com>
2015-11-17 18:32:32 -0500

    Moved product models to the models package and added search() and add_price placeholders.

--- src/opt/Savvy/backend/models/products.py
+++ src/opt/Savvy/backend/models/products.py
@@ -1,1 +1,23 @@
 __author__ = 'Colin'
+
+
+class Product(object):
+    """Class for products."""
+
+    def __init__(self, product_id=None, description=None, tags=None, price_reliability=None):
+        self.product_id = product_id
+        self.description = description
+        self.tags = tags
+        self.price_reliability = price_reliability
+
+
+class ProductDB(object):
+    """Class to connect to the Products Datastore."""
+
+    def search(self, query):
+        """Returns a list of matching products."""
+        pass
+
+    def add_price(self):
+        """Adds a price record to the database."""
+        pass
