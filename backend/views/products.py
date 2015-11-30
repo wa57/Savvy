176574be3de03e5c8328531eb20396422500d138
Colin McIntosh <colin@colinmcintosh.com>
2015-11-29 20:08:57 -0500

    Added average price.

--- backend/views/products.py
+++ backend/views/products.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from flask import request
 
 from backend.models.products import ProductDB
+from backend.models.prices import PriceDB
 from backend.utils import json_error, crossdomain
 
 product_blueprint = Blueprint("products", __name__, url_prefix="/api/v1/products")
@@ -36,5 +37,9 @@ def api_search():
     if not query:
         return json_error("Query can not be empty")
     product_db = ProductDB()
-    results = product_db.search(query)
+    price_db = PriceDB()
+    results = []
+    for result in product_db.search(query):
+        result["average_price"] = price_db.average_price(result["description"])
+        results.append(result)
     return json.dumps(results)
