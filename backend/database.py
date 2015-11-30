21abe4a25c496bcfef2df51eed7d70e19b352c9e
Colin McIntosh <colin@colinmcintosh.com>
2015-11-29 20:08:57 -0500

    Added average price.

--- backend/database.py
+++ backend/database.py
@@ -5,3 +5,10 @@ from pymongo import MongoClient
 
 mongo = MongoClient(host=config.MONGO_HOST)
 db = mongo[config.MONGO_DATABASE]
+
+
+class DB(object):
+    """Database parent class."""
+
+    def __init__(self):
+        self.db = db
--- backend/models/prices.py
+++ backend/models/prices.py
@@ -1,28 +1,46 @@
 __author__ = 'Colin'
 
+from backend.database import DB
 
-class PriceDB(object):
+
+class PriceDB(DB):
     """Class to connect to the Prices Datastore."""
+
+    def average_price(self, product):
+        """Returns the average price of a product."""
+        result = self.db.prices.aggregate([
+            {"$match":
+                {
+                    "product": product
+                }
+            },
+            {"$group":
+                {
+                    "_id": "$product",
+                    "average": {"$avg": "$price"}
+                }
+            }
+        ])
+        return int(next(result)["average"])
+
 
     def search(self, product=None, business=None):
         """Returns a list of matching prices."""
         import re
-        from backend.database import db
         query = {}
         if product:
             query["product"] = re.compile(".*{}.*".format(product), re.IGNORECASE)
         if business:
             query["business"] = re.compile(".*{}.*".format(business), re.IGNORECASE)
-        results = db.prices.find(query, {"_id": 0})
+        results = self.db.prices.find(query, {"_id": 0})
         return [dict(result) for result in results]
 
     def add_price(self, product, business, price, user):
         """Adds a price record to the database."""
-        from backend.database import db
         new_price = {"product": product,
                      "business": business,
                      "price": int(price),
                      "user": user}
-        result = db.prices.insert_one(new_price)
+        result = self.db.prices.insert_one(new_price)
         return result.inserted_id or None
 
