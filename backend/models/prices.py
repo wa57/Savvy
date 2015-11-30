9d5dc4573d307e83178edc331c07b0d72fa9960b
Colin McIntosh <colin@colinmcintosh.com>
2015-11-29 23:14:30 -0500

    Fixed add_price timestamp

--- backend/models/prices.py
+++ backend/models/prices.py
@@ -42,7 +42,8 @@ class PriceDB(DB):
                      "business": business,
                      "price": int(price),
                      "user": user,
-                     "submitted_timestamp": Timestamp(datetime.now())}
+                     "submitted_timestamp": Timestamp(datetime.now(), 1)}
         result = self.db.prices.insert_one(new_price)
+
         return result.inserted_id or None
 
