ca23db887850b08604f2b7bdbd084b863a12954f
Colin McIntosh <colin@colinmcintosh.com>
2016-01-12 16:31:36 -0500

    Added get product by ID and fixed errors caused by items being in the product database but not in the price database.

--- backend/views/products.py
+++ backend/views/products.py
@@ -10,6 +10,34 @@ from backend.utils import json_error, crossdomain
 product_blueprint = Blueprint("products", __name__, url_prefix="/api/v1/products")
 
 
+@product_blueprint.route("/<product_id>", methods=["GET"])
+@crossdomain(origin="*")
+def api_get_product(product_id):
+    """Returns details about a single product.
+
+    Input Parameters:
+        product_id       (string): The product ID to get.
+
+    Returns:
+        A JSON object of dictionaries containing details for the product matching the ID.
+
+        Example:
+            {
+                product_id: 1235,
+                description: "Large Coffee",
+                tags: ["coffee", "drinks", "caffeine"]
+            }
+    """
+    product_db = ProductDB()
+    price_db = PriceDB()
+    result = product_db.get(product_id)
+    result["average_price"] = price_db.average_price(result["description"])
+    result["lowest_price"] = price_db.lowest_price(result["description"])
+    result["highest_price"] = price_db.highest_price(result["description"])
+    result["average_price_per_day"] = price_db.average_price_per_day(result["description"])
+    return json.dumps(result)
+
+
 @product_blueprint.route("/search", methods=["GET"])
 @crossdomain(origin="*")
 def api_search():
