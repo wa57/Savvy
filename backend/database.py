00b96c4a123c99a9365b0238ab29b4126415a212
Colin McIntosh <colin@colinmcintosh.com>
2016-02-10 20:04:22 -0500

    Shortened timeout times for database.

--- backend/database.py
+++ backend/database.py
@@ -9,7 +9,7 @@ logger = logging.getLogger("savvy.database")
 
 
 logger.debug("Connecting to database...")
-mongo = MongoClient(host=config.MONGO_HOST, socketTimeoutMS=2500, connectTimeoutMS=1000)
+mongo = MongoClient(host=config.MONGO_HOST, socketTimeoutMS=2000, connectTimeoutMS=1000)
 db = mongo[config.MONGO_DATABASE]
 logger.debug("Connected to database.")
 
