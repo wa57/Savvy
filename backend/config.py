82bf6934688c8aef700a2cbb2ec44f4f13573e1e
Colin McIntosh <colin@colinmcintosh.com>
2016-06-03 16:16:34 -0400

    Added blank config file.

---
+++ backend/config.py
@@ -0,0 +1,14 @@
+from datetime import timedelta
+
+
+SAVVY_LOGIN_EXPIRATION = timedelta(hours=24)
+
+MONGO_HOST = ""
+MONGO_DATABASE = ""
+MONGO_USER = ""
+MONGO_PASSWORD = ""
+
+GOOGLE_CLOUD_VISION_KEY = "backend/keys/<somefile>.json"
+
+GMAIL_USERNAME = ""
+GMAIL_PASSWORD = ""
